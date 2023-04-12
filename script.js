b45fa81bb068a656fc0c46a4fe937eb754947847
Malte <maltebjoerklund@gmail.com>
2023-04-12 17:05:10 +0200

    Styling bars, fill and display array

--- script.js
+++ script.js
@@ -2,4 +2,28 @@
 
 window.addEventListener("load", start);
 
-function start() {}
+const numArray = [];
+
+function start() {
+  fillArray();
+  displayBars();
+  console.log(numArray);
+}
+
+function getNumberOfCustomers() {
+  return Math.floor(Math.random() * 32);
+}
+
+function fillArray() {
+  for (let i = 0; i < 40; i++) {
+    numArray[i] = getNumberOfCustomers();
+  }
+}
+
+function displayBars() {
+  const bars = document.querySelectorAll(".bar");
+  for (let i = 0; i < 40; i++) {
+    const bar = bars[i];
+    bar.style.height = `${numArray[i] * 10}px`;
+  }
+}
